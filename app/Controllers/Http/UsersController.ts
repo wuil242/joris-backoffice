306ab3e13f20d2cba0288789edcd532ded91fece
bouss <bousswuil@gmail.com>
2021-10-13 11:32:17 +0100

    (update) connexion avec regeneration de token

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import {schema, rules} from '@ioc:Adonis/Core/Validator'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class UsersController {
+  private TOKEN_TABLE = 'api_tokens'
 
   async login({request, auth, response}:HttpContextContract) {
     try {
@@ -11,13 +13,22 @@ export default class UsersController {
       })
   
       const payload = await request.validate({schema: validation})
+      const user = await auth.verifyCredentials(payload.email, payload.password)
+
+      if(user) {
+        const lastTokens = await Database.query().from(this.TOKEN_TABLE).where('user_id', user.id)
+        for (const token of lastTokens) {
+          await Database.from(this.TOKEN_TABLE).where('id', token.id).delete()
+        }
+      }
+
       const {token} = await auth.attempt(payload.email, payload.password)
       
       return {
         type: 'success',
         typeCode: 1,
         message: 'Login Successfull',
-        user: {...auth.user?.serialize({fields: ['name', 'email']}), token}
+        user: {...user?.serialize({fields: ['name', 'email']}), token}
       }
     }catch({messages}) {
       if(messages) {
