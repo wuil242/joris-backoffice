0d9ed19a56cabf70eed94f21e52e6470e9085624
bouss <bousswuil@gmail.com>
2021-10-13 23:51:57 +0100

    (rework) traitement des quratiers dans un composant dédié

--- resources/vue/components/locations/Location.vue
+++ resources/vue/components/locations/Location.vue
@@ -10,12 +10,10 @@
         @select="data.currentArrondissement = $event"
       ></arrondissements>
       
-      <!-- <quaters
-        :quaters="data.quaters"
-        @add="add_quater"
-        @select="data.quaters.current = $event"
-      >
-      </quaters> -->
+      <quaters
+        :city-id="data.currentCity" 
+        :arrondissement-id="data.currentArrondissement"
+      ></quaters>
     </section>
   </div>
   <!-- <nav>
@@ -36,83 +34,17 @@
 
 <script setup>
 
-import {reactive, onMounted} from 'vue'
-import FetchApi from '../../utils/FetchApi';
+import {reactive} from 'vue'
 import Cities from './Cities.vue'
 import Arrondissements from './Arrondissements.vue'
 import Quaters from './Quaters.vue'
-import { useStore } from 'vuex';
 
 const data = reactive({
   route: 'city',
   currentCity: 0,
-  currentArrondissement: 0,
-  arrondissements: {
-    current: 0,
-    elements: []
-  },
-  quaters: {
-    current: 0,
-    elements: []
-  }
+  currentArrondissement: 0
 })
 
-const store = useStore()
-
-/**
- * @param {string} name
- */
-function add_quater(name) {
-  FetchApi(
-    `/cities/${data.cities.current}/arrondissements/${data.arrondissements.current}/quaters`, 
-    'POST',
-    {name},
-    false
-  ).then(res => {
-    if(res.quaters) {
-      data.quaters.elements = res.quaters
-      data.quaters.current = res.quaters[0].id
-      get_quaters(data.arrondissements.current, data.quaters.current)
-    }
-  })
-}
-
-/**
- * @param {number} arrondissementId
- * @param {null|number} id
- */
-function get_quaters(arrondissementId, id = null) {
-    console.log(arrondissementId)
-   FetchApi(`/cities/${data.currentCity}/arrondissements/${arrondissementId}/quaters`)
-    .then(res => {
-      data.quaters.elements = res
-      data.currentArrondissement = arrondissementId
-       if(res.length > 0) {
-         data.quaters.current = id || res[0].id
-       }
-    })
-}
-
-
-
-/**
- * @param {number} cityId
- * @param {null|number} id
- */
-function get_arrondissements(cityId, id = null) {
-  
-  console.log(data.currentCity)
-  // FetchApi(`/cities/${cityId}/arrondissements`)
-  //   .then(res => {
-  //     data.arrondissements.elements = res
-  //     data.currentCity = cityId
-  //      if(res.length > 0) {
-  //        data.arrondissements.current = res[0].id
-  //        get_quaters(id || data.arrondissements.current)
-  //      }
-  //   })
-}
-  
 </script>
 
 <style lang="scss" scoped>
--- resources/vue/components/locations/Quaters.vue
+++ resources/vue/components/locations/Quaters.vue
@@ -3,11 +3,12 @@
     <location-layout 
       label="Nom de l'arrondissement" 
       submitMessage="ajouter une ville"
-      :locations="quaters"
+      :locations="data.quaters"
+      :current="data.currentQuater"
       border-color="gold"
-      @add="$emit('add', $event)"
-      @select="$emit('select', $event)"
-      @delete="$emit('delete', $event)"
+      @add="add_quater"
+      @select="data.currentQuater = $event"
+      @delete="remove_quater"
     >
       <h2 slot="title">Ajouter Un arrondissement</h2>
       <!-- <h2 slot="location-title">Toutes les Villes</h2> -->
@@ -19,13 +20,78 @@
 
 <script setup>
 import LocationLayout from './LocationLayout.vue';
+import {reactive, onUpdated} from 'vue'
+import FetchApi from '../../utils/FetchApi';
 
-defineProps({
-  quaters: {type: Object, required: true}
+
+const $props = defineProps({
+  cityId: {type:Number, required: true},
+  arrondissementId: {type:Number, required: true}
 })
 
-defineEmits(['add', 'select', 'delete'])
-  
+
+const $emit = defineEmits(['add', 'select', 'delete'])
+
+const data = reactive({
+  quaters: {
+    elements: [],
+  },
+  errors: [],
+  lastCity: 0,
+  lastArrondissement: 0,
+  currentQuater: 0,
+})
+
+onUpdated(() => {
+  if(data.lastArrondissement !== $props.arrondissementId) {
+    console.log($props.arrondissementId, $props.cityId)
+    data.lastArrondissement = $props.arrondissementId
+    data.lastCity = $props.cityId
+    get_quaters(data.lastArrondissement)
+  }
+})
+
+/**
+ * @param {string} id
+ */
+function remove_quater(id) {
+  console.log('REMOVE QUATER [%d]', id)
+}
+
+
+/**
+ * @param {string} name
+ */
+function add_quater(name) {
+  FetchApi(
+    `/cities/${data.lastCity}/arrondissements/${data.lastArrondissement}/quaters`, 
+    'POST',
+    {name},
+    false
+  ).then(res => {
+    if(res.quaters) {
+      data.quaters.elements = res.quaters
+      data.currentQuater = res.quaters[0].id
+      // get_quaters(data.lastArrondissement, data.currentQuater)
+    }
+  })
+}
+
+/**
+ * @param {number} arrondissementId
+ * @param {null|number} id
+ */
+function get_quaters(arrondissementId, id = null) {
+   FetchApi(`/cities/${data.lastCity}/arrondissements/${arrondissementId}/quaters`)
+    .then(res => {
+      data.quaters.elements = res
+       if(res.length > 0) {
+         data.currentQuater = id || res[0].id
+       }
+    })
+}
+
+
 </script>
 
 <style lang="scss" scoped>
