0372e5442b8bbb05203a9836baa17459b3d77223
bouss <bousswuil@gmail.com>
2021-10-17 13:21:02 +0100

    (add) boutton du mode nuit

---
+++ resources/vue/components/DarkModeButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="store.dispatch('toggle')">
+    Dark Mode <i :class="store.state.mode === '' ? 'fa' : 'far' " class="fa-moon"></i>
+  </button>
+</template>
+
+
+<script setup>
+import { useStore } from 'vuex';
+
+
+const store = useStore()
+  
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
