04f5b9c1f5b649d8adeed0d313b34bf5bf856e3d
bouss <bousswuil@gmail.com>
2021-10-17 11:34:31 +0100

    (rework) tout le routage dans un fichier

--- app/routes/Locations.ts
+++ app/routes/Locations.ts
@@ -1,1 +1,34 @@
-import './Cities'
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+
+  Route.get('/', 'LocationsController.getAllCities')
+
+  Route.post('/', 'LocationsController.storeCity')
+
+  
+  Route.delete('/', 'LocationsController.destroyCity')
+
+  Route.get('/:id/arrondissements', 'LocationsController.getArrondissements')
+    .where('id', Route.matchers.number())
+
+  Route.post('/:id/arrondissements', 'LocationsController.storeArrondissement')
+    .where('id', Route.matchers.number())
+
+
+  Route.delete('/:cityId/arrondissements', 'LocationsController.destroyArrondissement')
+    .where('cityId', Route.matchers.number())
+
+  Route.get('/:cityId/arrondissements/:arrondissementId/quaters', 'LocationsController.getQuaters')
+    .where('cityId', Route.matchers.number())
+    .where('arrondissementId', Route.matchers.number())
+
+  Route.post('/:cityId/arrondissements/:arrondissementId/quaters', 'LocationsController.storeQuaters')
+    .where('cityId', Route.matchers.number())
+    .where('arrondissementId', Route.matchers.number())
+
+  Route.delete('/:cityId/arrondissements/:arrondissementId/quaters', 'LocationsController.destroyQuaters')
+    .where('cityId', Route.matchers.number())
+    .where('arrondissementId', Route.matchers.number())
+
+}).prefix('/api/cities')
