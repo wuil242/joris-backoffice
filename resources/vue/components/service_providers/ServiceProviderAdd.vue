58181d5a949eb6557f1508216f34f62430640757
bouss <bousswuil@gmail.com>
2021-10-21 16:47:59 +0100

    (add) champ de selection du job du prestataire

--- resources/vue/components/service_providers/ServiceProviderAdd.vue
+++ resources/vue/components/service_providers/ServiceProviderAdd.vue
@@ -1,40 +1,51 @@
 <template>
   <h1>ADD -SP</h1>
+  {{form}}
   <form-custom @submit="add_sp">
-    <form-group legend="personal infos">
-      <input-custom type="radio" label="Sexe" :value="{Homme: 'm', Femme: 'f'}" @change="log"></input-custom>
+    <form-group legend="Infos Personnel">
+      <input-custom placeholder="Nom" @keyup="form.personal.lastname = $event"></input-custom>
+      <input-custom placeholder="Prenom" @keyup="form.personal.firstname = $event"></input-custom>
+      <input-custom type="date" label="date de naissance" @change="form.personal.date = $event"></input-custom>
+      <input-custom
+        type="radio"
+        label="sexe"
+        :value="{ Homme: 'm', Femme: 'f' }"
+        @change="form.personal.sexe = $event"
+      ></input-custom>
       <select-custom
-        label="Ville" 
-        :options="options" 
-        key-option="name"
+        :options="[{ job: 'Cool', value: 10 }]"
+        key-option="job"
         key-value="value"
+        @change="form.personal.job = $event"
       ></select-custom>
-       <select-custom
-        label="Quartiers"
-        :option-default="{name: 'Aucun', value: 0}" 
-        :options="options" 
-        key-option="name"
-        key-value="value"
-      ></select-custom>
+      <input-custom
+        type="textarea"
+        placeholder="introduction"
+        @keyup="form.personal.introduce = $event"
+      ></input-custom>
+      <input-custom
+        type="textarea"
+        placeholder="phrase d'accroche"
+        @keyup="form.personal.arrcroch_sentence = $event"
+      ></input-custom>
     </form-group>
-    <fom-submit-button value="Ajouter"></fom-submit-button>
+    <!-- <input type="submit"  value="SEND"/> -->
+    <input-custom type="submit" value="SEND"></input-custom>
   </form-custom>
 </template>
 
 
 <script setup>
-import FormCustom from '../form/FormCustom.vue'
+
+import { reactive } from 'vue'
+import FormCustom from '../form/FormCustom.vue';
 import FormGroup from '../form/FormGroup.vue';
 import InputCustom from '../form/InputCustom.vue';
 import SelectCustom from '../form/SelectCustom.vue';
 import FomSubmitButton from '../form/FomSubmitButton.vue';
 
-import {reactive} from 'vue'
-import FormCustom from '../proto/FormCustom.vue';
-import InputCustom from '../form/InputCustom.vue';
-
 const form = reactive({
-  personal : {
+  personal: {
     lastname: '',
     firstname: '',
     date: '',
@@ -52,21 +63,20 @@ const form = reactive({
 function add_sp() {
   console.log('ADD')
 }
-  
-  
+
+
 </script>
 
 <style lang="scss" scoped>
-  form {
-    width: 50%;
-  }
+form {
+  width: 50%;
+}
 
-  fieldset {
-    display: flex;
-    flex-direction: column;
-    gap: .25em;
+fieldset {
+  display: flex;
+  flex-direction: column;
+  gap: 0.25em;
 
-    padding: .5em;
-
-  }
+  padding: 0.5em;
+}
 </style>
