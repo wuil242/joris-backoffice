3ece6ae90fae43dd3a7c7846ee97c4674cd1333a
bouss <bousswuil@gmail.com>
2021-10-21 14:20:17 +0100

    (add) champ pour les infos personelles

--- resources/vue/components/service_providers/ServiceProviderAdd.vue
+++ resources/vue/components/service_providers/ServiceProviderAdd.vue
@@ -1,36 +1,17 @@
 <template>
   <h1>ADD -SP</h1>
-  {{form}}
   <form-custom @submit="add_sp">
-    <form-group legend="Infos Personnel">
+    <fieldset >
+      <legend>Infos Personnel</legend>
       <input-custom placeholder="Nom" @keyup="form.personal.lastname = $event"></input-custom>
       <input-custom placeholder="Prenom" @keyup="form.personal.firstname = $event"></input-custom>
       <input-custom type="date" label="date de naissance" @change="form.personal.date = $event"></input-custom>
-      <input-custom
-        type="radio"
-        label="sexe"
-        :value="{ Homme: 'm', Femme: 'f' }"
-        @change="form.personal.sexe = $event"
-      ></input-custom>
-      <select-custom
-        :options="[{ job: 'Cool', value: 10 }]"
-        key-option="job"
-        key-value="value"
-        @change="form.personal.job = $event"
-      ></select-custom>
-      <input-custom
-        type="textarea"
-        placeholder="introduction"
-        @keyup="form.personal.introduce = $event"
-      ></input-custom>
-      <input-custom
-        type="textarea"
-        placeholder="phrase d'accroche"
-        @keyup="form.personal.arrcroch_sentence = $event"
-      ></input-custom>
-    </form-group>
-    <!-- <input type="submit"  value="SEND"/> -->
-    <input-custom type="submit" value="SEND"></input-custom>
+      <input-custom type="radio" label="sexe" :value="{Homme: 'm', Femme: 'f'}" @change="form.personal.sexe = $event"></input-custom>
+      <!-- ajout du champ de selection multiple pur les Job de prestataire  -->
+      <input-custom type="textarea" placeholder="introduction" @keyup="form.personal.introduce = $event"></input-custom>
+      <input-custom type="textarea" placeholder="phrase d'accroche" @keyup="form.personal.arrcroch_sentence = $event"></input-custom>
+    </fieldset>
+    <button type="submit">SEND</button>
   </form-custom>
 </template>
 
@@ -44,8 +25,12 @@ import InputCustom from '../form/InputCustom.vue';
 import SelectCustom from '../form/SelectCustom.vue';
 import FomSubmitButton from '../form/FomSubmitButton.vue';
 
+import {reactive} from 'vue'
+import FormCustom from '../proto/FormCustom.vue';
+import InputCustom from '../form/InputCustom.vue';
+
 const form = reactive({
-  personal: {
+  personal : {
     lastname: '',
     firstname: '',
     date: '',
@@ -63,20 +48,20 @@ const form = reactive({
 function add_sp() {
   console.log('ADD')
 }
-
-
+  
 </script>
 
 <style lang="scss" scoped>
-form {
-  width: 50%;
-}
+  form {
+    width: 50%;
+  }
 
-fieldset {
-  display: flex;
-  flex-direction: column;
-  gap: 0.25em;
+  fieldset {
+    display: flex;
+    flex-direction: column;
+    gap: .25em;
 
-  padding: 0.5em;
-}
+    padding: .5em;
+
+  }
 </style>
