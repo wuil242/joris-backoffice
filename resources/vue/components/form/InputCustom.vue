eee5b3d082168e9addcc9d1236200f3c7c051d44
bouss <bousswuil@gmail.com>
2021-10-21 14:59:15 +0100

    (add) radio generer automatiquement

--- resources/vue/components/form/InputCustom.vue
+++ resources/vue/components/form/InputCustom.vue
@@ -1,44 +1,65 @@
 <template>
   <template v-if="type === 'textarea'">
-    <label>{{label}}</label>
-    <textarea 
-      @keyup="$emit('keyup', value)" 
+    <label>{{ label }}</label>
+    <textarea
+      @keyup="$emit('keyup', input_value)"
       :placeholder="placeholder"
       :required="required"
-      v-model="value"
+      v-model="input_value"
     ></textarea>
   </template>
+  <template v-else-if="type === 'radio'">
+    <p>{{ label }}</p>
+    <template v-for="(val, key) in value" :key="val">
+      <label :for="key + val">{{ key }}</label>
+      <input
+        type="radio"
+        :name="label"
+        :value="val"
+        :id="key + val"
+        v-model="input_value"
+        @change="$emit('change', input_value)"
+      />
+    </template>
+  </template>
   <template v-else>
-    <label>{{label}}</label>
-    <input 
-    :type="type" 
-    :placeholder="placeholder"
-    :required="required"
-    @keyup="$emit('keyup', value)"
-    @change="$emit('change', value)"
-    v-model="value"
+    <label>{{ label }}</label>
+    <input
+      :type="type"
+      :placeholder="placeholder"
+      :required="required"
+      @keyup="$emit('keyup', input_value)"
+      @change="$emit('change', input_value)"
+      v-model="input_value"
     />
   </template>
 </template>
 
 
 <script setup>
-
+/**
+ *----
+ * Pour les input de type "radio" la proprité value est de type
+ * value: {[name]:[value]}
+ *----
+ */
 defineProps({
-  type: {type:String, default: 'text'},
-  label: {type:String, default: ''},
-  placeholder: {type:String, default: ''},
-  required: {default: null}
+  type: { type: String, default: 'text' },
+  label: { type: String, default: '' },
+  placeholder: { type: String, default: '' },
+  required: { default: null },
+  value: { type: [String, Object], default: '' }
 })
 
 defineEmits(['keyup', 'change'])
 
-const value = ''
-  
+const input_value = ''
+
 </script>
 
 <style lang="scss" scoped>
-  input, textarea {
-    padding: .25em;
-  }
+input,
+textarea {
+  padding: 0.25em;
+}
 </style>
--- resources/vue/components/service_providers/ServiceProviderAdd.vue
+++ resources/vue/components/service_providers/ServiceProviderAdd.vue
@@ -2,6 +2,7 @@
   <h1>ADD -SP</h1>
   <form-custom>
     <form-group legend="personal infos">
+      <input-custom type="radio" label="Sexe" :value="{Homme: 'm', Femme: 'f'}" @change="log"></input-custom>
     </form-group>
   </form-custom>
 </template>
@@ -10,6 +11,11 @@
 <script setup>
 import FormCustom from '../form/FormCustom.vue'
 import FormGroup from '../form/FormGroup.vue';
+import InputCustom from '../form/InputCustom.vue';
+
+function log(data) {
+  console.log(data)
+}
   
 </script>
 
