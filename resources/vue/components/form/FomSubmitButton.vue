bfcbbcbe1175250e338da46fa5e36b0ec3651950
bouss <bousswuil@gmail.com>
2021-10-23 09:03:09 +0100

    (add) boutton de soumission du formulaire

---
+++ resources/vue/components/form/FomSubmitButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <input type="submit" :value="value">  
+</template>
+
+
+<script setup>
+
+defineProps({
+  value:String
+})
+
+</script>
+
+<style scoped>
+  
+</style>
--- resources/vue/components/service_providers/ServiceProviderAdd.vue
+++ resources/vue/components/service_providers/ServiceProviderAdd.vue
@@ -1,15 +1,17 @@
 <template>
   <h1>ADD -SP</h1>
-  <form-custom>
+  <form-custom @submit="log">
     <form-group legend="personal infos">
       <input-custom type="radio" label="Sexe" :value="{Homme: 'm', Femme: 'f'}" @change="log"></input-custom>
-      <select-custom 
+      <select-custom
+        label="Ville" 
         :options="options" 
         key-option="name"
         key-value="value"
         @change="log"
       ></select-custom>
        <select-custom
+        label="Quartiers"
         :option-default="{name: 'Aucun', value: 0}" 
         :options="options" 
         key-option="name"
@@ -17,6 +19,7 @@
         @change="log"
       ></select-custom>
     </form-group>
+    <fom-submit-button value="Ajouter"></fom-submit-button>
   </form-custom>
 </template>
 
@@ -26,6 +29,7 @@ import FormCustom from '../form/FormCustom.vue'
 import FormGroup from '../form/FormGroup.vue';
 import InputCustom from '../form/InputCustom.vue';
 import SelectCustom from '../form/SelectCustom.vue';
+import FomSubmitButton from '../form/FomSubmitButton.vue';
 
 function log(data) {
   console.log(data)
