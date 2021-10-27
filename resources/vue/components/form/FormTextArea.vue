8699977dd6434aae9bc5ac84a42f65572edd8252
bouss <bousswuil@gmail.com>
2021-10-27 13:38:33 +0100

    (add) composant de champ à texte multiple

---
+++ resources/vue/components/form/FormTextArea.vue
@@ -0,0 +1,35 @@
+<template>
+    <label>{{ label }}</label>
+    <textarea
+      :placeholder="placeholder"
+      :required="required"
+      :value="value"
+      @input="$emit('update:value', $event.target.value)"
+    ></textarea>
+</template>
+
+
+<script setup>
+/**
+ *----
+ * Pour les input de type "radio" la proprité value est de type
+ * value: {[name]:[value]}
+ *----
+ */
+defineProps({
+  label: { type: String, default: '' },
+  placeholder: { type: String, default: '' },
+  required: { default: null },
+  value:String
+})
+
+defineEmits(['update:value'])
+
+</script>
+
+<style lang="scss" scoped>
+input,
+textarea {
+  padding: 0.25em;
+}
+</style>
