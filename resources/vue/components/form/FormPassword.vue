d87a175d8ffe132767d7cd45206409cbb4308000
bouss <bousswuil@gmail.com>
2021-10-30 22:53:53 +0100

    (add) composant pour les champs de type mot de passe

---
+++ resources/vue/components/form/FormPassword.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="form-password">
+    <form-input :value="value" :type="data.type" :label="label" :placeholder="placeholder" @update:value="$emit('update:value', $event)">
+    </form-input>
+      <font-awesome class="form-password-icon" type="far" cursor="pointer" :icon="data.icon" @click="show_or_hide_password"></font-awesome>
+  </div>
+</template>
+
+
+<script setup>
+import { reactive } from 'vue';
+import FontAwesome from '../FontAwesome.vue';
+import FormInput from './FormInput.vue';
+
+defineProps({
+  label: { type: String, default: '' },
+  placeholder: { type: String, default: '' },
+  value: String
+})
+
+defineEmits(['update:value'])
+
+const data = reactive({
+  type: 'password',
+  icon: 'eye-slash'
+})
+
+function show_or_hide_password() {
+  data.type = data.type === 'text' ? 'password' : 'text'
+  data.icon = data.icon === 'eye' ? 'eye-slash' : 'eye'
+}
+
+</script>
+
+<style scoped>
+.form-password {
+  position: relative;
+  width: 50%;
+}
+
+.form-password-icon {
+  position: absolute;
+  top: 25%;
+  right: 1%;
+  color: grey;
+}
+</style>
