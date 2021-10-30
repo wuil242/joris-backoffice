234ff7d1fd30c9941cbc44d900ff231587e4138f
bouss <bousswuil@gmail.com>
2021-10-30 23:18:30 +0100

    (add) composant des erreurs de formulaires

---
+++ resources/vue/components/form/FormErrors.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="messages.length > 0">
+    <template v-for="message in messages">
+        <span class="form-error">{{message}}</span>
+    </template>
+  </div>
+</template>
+
+
+<script setup>
+defineProps({
+  messages: Array,
+  color: {type: String, default: '#F32727'}
+})
+  
+</script>
+
+<style scoped>
+  .form-error {
+    color: v-bind(color);
+    display: block;
+  }
+
+</style>
